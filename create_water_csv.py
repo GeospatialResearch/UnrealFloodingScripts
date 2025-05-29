210f5880e4ced9bc51d1c438f227d22123eb6a3c
Luke Parkinson <luke.parkinson@canterbury.ac.nz>
2025-05-30 09:56:25 +1200

    Add comments and docstrings

--- create_water_csv.py
+++ create_water_csv.py
@@ -1,3 +1,5 @@
+"""Script to read flood model output and points file to create a list of water sources in csv format."""
+
 from pathlib import Path
 from typing import Dict, List
 
@@ -7,33 +9,40 @@ import rioxarray as rxr
 from shapely.geometry import Point
 import xarray as xr
 
-from models import Vector, WaterSource
-
-GAUGE_POINTS_PATH = Path(r"D:\unreal\taumutu\Data\Vector\read_points_1.geojson")
+# Path to the file of point locations to query.
+GAUGE_POINTS_PATH = Path(r"D:\unreal\taumutu\Data\Vector\read_points_002.geojson")
+# Path to the Flood model output .nc file
 FLOOD_MODEL_OUTPUT_PATH = Path(r"D:\unreal\taumutu\Data\taumutu_100_year.nc")
+# Path to the bounding box of the unreal level, may be smaller than the flood model output
 UNREAL_LEVEL_BOUNDS_PATH = Path(r"D:\unreal\taumutu\Data\AoI\Taumutu_SW_2k.geojson")
+# Path to the output of this script
 WATER_SOURCES_OUTPUT_PATH = Path(r"output.csv")
+# Whether points should snap to the same grid as the flood model output, or allow arbitrary floating point locations
 LOCK_TO_GRID = True
 
 
 def get_gauge_points() -> gpd.GeoDataFrame:
+    """
+    Read the file of point locations to be used for querying.
+
+    :return gpd.GeoDataFrame The point locations.
+    """
     return gpd.read_file(GAUGE_POINTS_PATH)
 
 
-def convert_depths_to_water_sources(gauge_depths: gpd.GeoDataFrame) -> List[WaterSource]:
-    geometry = gauge_depths.geometry
-    depth_column = gauge_depths.iloc[:, 3]  # Get column index 3 as an example of d
-    water_sources = [
-        WaterSource(location=Vector(x, y), volume=d) for x, y, d in zip(geometry.x, geometry.y, depth_column)
-    ]
-    return water_sources
-
-
 def export_to_csv(gauge_depths: gpd.GeoDataFrame, out_file_path: Path) -> None:
+    """
+    Take the gauge_depth dataframe and export it to a csv file.
+
+    :param gauge_depths gpd.GeoDataFrame The points including Z and data on their depths and times.
+    :param out_file_path Path The output csv file.
+    """
     gauge_depths["x"] = gauge_depths.geometry.x
     gauge_depths["y"] = gauge_depths.geometry.y
-    # Reorder x and y columns to beginning
+    gauge_depths["z"] = gauge_depths.geometry.z
+    # Reorder x,y,z  columns to beginning
     columns = list(gauge_depths)
+    columns.insert(0, columns.pop(columns.index("z")))
     columns.insert(0, columns.pop(columns.index("y")))
     columns.insert(0, columns.pop(columns.index("x")))
     gauge_depths = gauge_depths.loc[:, columns]
@@ -43,6 +52,13 @@ def export_to_csv(gauge_depths: gpd.GeoDataFrame, out_file_path: Path) -> None:
 
 
 def transform_gdf_to_unreal_coordinates(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    """
+    Converts a gpd.GeoDataFrame from CRS in metres to a gpd.GeoDataFrame using unreal CRS (metres, centre of level origin).
+
+    :param gdf: gpd.GeoDataFrame The gdf to be transformed.
+
+    :return gpd.GeoDataFrame The gpd.GeoDataFrame after transformation.
+    """
     unreal_world_bounds = gpd.read_file(UNREAL_LEVEL_BOUNDS_PATH)
     unreal_origin_location = unreal_world_bounds.geometry.centroid
 
@@ -57,9 +73,16 @@ def transform_gdf_to_unreal_coordinates(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFra
     return gdf
 
 
-def extract_depths_for_single_point(row: gpd.GeoSeries, depth_array: xr.DataArray) -> gpd.GeoSeries:
-    point_depth_array = depth_array.sel(xx_P0=row.geometry.x, yy_P0=row.geometry.y, method="nearest")
-    times = point_depth_array.time.values
+def extract_depths_for_single_point(row: gpd.GeoSeries, model_output: xr.Dataset) -> gpd.GeoSeries:
+    """
+    Takes a single point and queries the model output to get depths and terrain elevation.
+    :param row: gpd.GeoSeries The point location.
+    :param model_output: xr.Dataset The flood model output
+    :return:  gpd.GeoSeries The depths and 3D point.
+    """
+    point_model_output = model_output.sel(xx_P0=row.geometry.x, yy_P0=row.geometry.y, method="nearest")
+    point_depth_array = point_model_output.h_P0
+    times = point_model_output.time.values
     depths_for_times = {}
     for time_slice in times:
         depths_for_times[str(time_slice)] = point_depth_array.sel(time=time_slice).values.item(0)
@@ -67,29 +90,34 @@ def extract_depths_for_single_point(row: gpd.GeoSeries, depth_array: xr.DataArra
         # Modify point to be centred on raster cell
         x = point_depth_array.xx_P0.values.item(0)
         y = point_depth_array.yy_P0.values.item(0)
-        depths_for_times["geometry"] = Point(x, y)
+        z = point_model_output.zb_P0.values.item(0)
+        depths_for_times["geometry"] = Point(x, y, z)
     row.update(depths_for_times)
     return row
 
 
-def extract_depths_for_points(gauge_points: gpd.GeoDataFrame, depth_array: xr.DataArray) -> gpd.GeoDataFrame:
+def extract_depths_for_points(gauge_points: gpd.GeoDataFrame, model_output: xr.Dataset) -> gpd.GeoDataFrame:
+    """
+    Takes many points and queries the model output to get depths and terrain elevation for each.
+    :param gauge_points: gpd.GeoDataFrame The point locations.
+    :param model_output: xr.Dataset The flood model output.
+    :return: gpd.GeoDataFrame The depths and 3D points.
+    """
     point_depths = gauge_points.copy()
-    time_slices = depth_array.time.values
+    time_slices = model_output.time.values
     for time_slice in time_slices:
         point_depths[str(time_slice)] = pd.Series(dtype='float32')
-    point_depths = point_depths.apply(lambda point: extract_depths_for_single_point(point, depth_array), axis=1)
+    point_depths = point_depths.apply(lambda point: extract_depths_for_single_point(point, model_output), axis=1)
     return point_depths
 
 
 def main():
+    """Script to read flood model output and points file to create a list of water sources in csv format."""
     gauge_points = get_gauge_points()
-    with xr.open_dataset(FLOOD_MODEL_OUTPUT_PATH, decode_coords="all") as ds:
-        depth_array = ds["h_P0"]
-        gauge_depths = extract_depths_for_points(gauge_points, depth_array)
+    with xr.open_dataset(FLOOD_MODEL_OUTPUT_PATH, decode_coords="all") as model_output:
+        gauge_depths = extract_depths_for_points(gauge_points, model_output)
     gauge_depths_unreal = transform_gdf_to_unreal_coordinates(gauge_depths)
-    water_sources = convert_depths_to_water_sources(gauge_depths_unreal)
     export_to_csv(gauge_depths_unreal, WATER_SOURCES_OUTPUT_PATH)
-    print(water_sources)
 
 
 if __name__ == '__main__':
--- spawn_water.py
+++ spawn_water.py
@@ -1,47 +1,110 @@
+"""Unreal script to spawn water sources at a location based on csv."""
+
 import csv
 import os
 import pathlib
 
 import unreal
 
-from models import Vector, WaterSource
+from dataclasses import dataclass
+from typing import ClassVar, List, NamedTuple
 
-def vector_to_unreal(vector: Vector) -> unreal.Vector:
+
+class Vector(NamedTuple):
+    """
+    Represents a vector in unreal coordinates, without 3rd party dependencies.
+    :arg x: The x component of the vector.
+    :arg y: The y component of the vector.
+    :arg z: The z component of the vector.
+    """
+    x: float
+    y: float
+    z: float = 0
+
+
+@dataclass(frozen=True)
+class WaterSource:
+    """
+    Represents a water source.
+    :arg blueprint_class_path: C;ass
+    """
+    blueprint_class_path: ClassVar[
+        str] = "/Game/FluidFlux/Simulation/Modifiers/BP_FluxModifierSourceActor.BP_FluxModifierSourceActor"
+    location: Vector = Vector(0, 0, 0)
+    volume: float = 1
+    intensity: float = 1
+    direction: Vector = Vector(0, 0)
+
+
+def vector_to_unreal(vector: Vector):
     return unreal.Vector(vector.x, vector.y, vector.z)
 
 
+def spawn_single_water_source(
+    water_source: WaterSource,
+    water_source_bp_class: unreal.BlueprintGeneratedClass,
+    water_modifier_bp_class: unreal.BlueprintGeneratedClass
+):
+    """
+    Spawns a single water source into an unreal level
+
+    :param water_source: WaterSource The Water Source to spawn
+    :param water_source_bp_class: unreal.BlueprintGeneratedClass The unreal blueprint class for a water source.
+    :param water_modifier_bp_class: unreal.BlueprintGeneratedClass The blueprint class for a modifier component.
+    """
+    location = vector_to_unreal(water_source.location)
+    rotation = (location - location).rotator()
+    # Spawn actor into level
+    source_actor = unreal.EditorLevelLibrary.spawn_actor_from_class(actor_class=water_source_bp_class,
+                                                                    location=location,
+                                                                    rotation=rotation)
+    # Move actor to specified subfolder
+    source_actor.set_folder_path("/FluidFlux/Sources")
+    # Modify the water parameters
+    modifier_component = source_actor.get_component_by_class(water_modifier_bp_class)
+    modifier_component.set_editor_property("volume", water_source.volume)
+    mode_enum = modifier_component.get_editor_property("mode")
+    modifier_component.set_editor_property("mode", mode_enum.SET)
+
+
 def spawn_water_sources(water_sources: List[WaterSource]):
+    """
+    Add a list of water sources into an unreal level.
+    :param water_sources: List[WaterSource] The Water Sources to spawn.
+    """
     EAL = unreal.EditorAssetLibrary
-    ELL = unreal.EditorLevelLibrary
     blueprint_class = EAL.load_blueprint_class(asset_path=WaterSource.blueprint_class_path)
     component_class = EAL.load_blueprint_class(
         asset_path="/Game/FluidFlux/Simulation/Modifiers/Components/BP_FluxModifierSourceComponent.BP_FluxModifierSourceComponent")
     for water_source in water_sources:
-        location = vector_to_unreal(water_source.location)
-        rotation = (location - location).rotator()
-        source_actor = ELL.spawn_actor_from_class(actor_class=blueprint_class,
-                                                  location=location,
-                                                  rotation=rotation)
-        source_actor.set_folder_path("/FluidFlux/Sources")
-        modifier_component = source_actor.get_component_by_class(component_class)
-        modifier_component.set_editor_property("volume", water_source.volume)
-        mode_enum = modifier_component.get_editor_property("mode")
-        modifier_component.set_editor_property("mode", mode_enum.SET)
+        spawn_single_water_source(water_source, blueprint_class, component_class)
 
 
 def read_water_sources_csv(csv_path: pathlib.Path) -> List[WaterSource]:
+    """
+    Read the csv file to get a list of water sources in unreal coordinates.
+    :param csv_path: Path to the csv file to read water sources from.
+    :return List[WaterSource]: A list of water sources in unreal coordinates.
+    """
     water_sources = []
     with open(csv_path, "r") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         next(csv_reader, None)  # Skip headers
-        for x, y, *zt in csv_reader:
-            water_sources.append(WaterSource(Vector(float(x) * 100, float(y) * 100), volume=float(zt[3]) * 100))
-
+        for x, y, z, *zt in csv_reader:  # Get each water source parameter from the csv
+            water_sources.append(
+                WaterSource(
+                    # Add a water source in cm units, with z adjusted to match unreal level scale
+                    Vector(float(x) * 100, float(y) * 100, (float(z) * 181.99 + 1913)),
+                    # Set volume to the depth of the water in a hand-picked time slice.
+                    volume=float(zt[3]) * 100
+                )
+            )
     return water_sources
 
 
 def main():
-    file_path = pathlib.Path(__file__).parent / "testing.csv"
+    """Unreal script to spawn water sources at a location based on csv."""
+    file_path = pathlib.Path(__file__).parent / "output.csv"
     water_sources = read_water_sources_csv(file_path)
     spawn_water_sources(water_sources)
 
